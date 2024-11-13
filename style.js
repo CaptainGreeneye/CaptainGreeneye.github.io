874dc5019f07cafe06e0049db7df464a7aeeffdd
CaptainGreeneye <last.normal.man@gmail.com>
2024-11-13 11:17:38 +0100

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,22 @@
+let currentSlide = 0;
 
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-slide');
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+function nextSlide() {
+    const slides = document.querySelectorAll('.carousel-slide');
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    const slides = document.querySelectorAll('.carousel-slide');
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Ініціалізувати перший слайд
+showSlide(currentSlide);
